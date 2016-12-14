415b8e73e7f550b9917ff2a4e14946a9f692b5f3
Radoslav Latincic <latincicra@RZS.RS>
2016-12-14 10:39:34 +0100

    Primjeri dodavanje

--- Radoslav16-ParametriMetoda/src/App.java
+++ Radoslav16-ParametriMetoda/src/App.java
@@ -1,6 +1,7 @@
 class Frog {
 	private String name;
 	private int age;
+	private String boja;
 	
 	public void setName(String name){
 		this.name=name;
@@ -8,15 +9,25 @@ class Frog {
 	public void setAge(int age){
 		this.age=age;
 	}
+	public void setBoja(String boja){
+		this.boja=boja;
+	}
+	public String getBoja(){
+		return boja;
+	}
 	public String getName(){
 		return name;
 	}
 	public int getAge(){
 		return age;
 	}
-	public void setInfo(String name, int age){
+	public void setInfo(String name, int age,String boja){
 		setName(name);
 		setAge(age);
+		setBoja(boja);
+	}
+	public void makeSound(){
+		System.out.println("Ja sam "+ this.name+". Kre, kre, sta ima novo?");
 	}
 }
 
@@ -25,12 +36,25 @@ public class App {
 	public static void main(String[] args) {
 		
 		Frog frog1 = new Frog();
+		Frog frog2=new Frog();
 		
 		frog1.setName("Zabac");
 		frog1.setAge(1);
+		frog1.setBoja("Zelen");
 		
-		System.out.println(frog1.getName());
+		frog2.setName("Kermit");
+		frog2.setAge(2);
+		frog2.setBoja("Zut");
+		
+		System.out.println(frog1.getBoja()+" "+frog1.getName());
+		
+		frog1.setInfo(frog1.getName(), frog1.getAge(),frog1.getBoja());
+		frog2.setInfo(frog2.getName(), frog2.getAge(),frog2.getBoja());
+		frog1.makeSound();
+		System.out.println(frog2.getBoja()+" "+frog2.getName());
+		frog2.makeSound();
 
 	}
 
+	
 }
